9918f92083add07334200b7114fd1a38275f4baa
ISNing <ISNing_SF@outlook.com>
2024-08-06 19:10:55 +0800

    feat(components): 添加带有文字的图像组件

---
+++ components/ImageWithHint.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    alt: String,
+    class: String,
+    src: String,
+})
+</script>
+
+<template>
+    <div class="text-center w-fit">
+        <img :src="src" :alt="alt" :class="class" />
+        {{ props.alt }}
+    </div>
+</template>
