e81500aa4702b3b95cf51dccb877f4738edc1728
ISNing <ISNing_SF@outlook.com>
2024-08-06 19:12:12 +0800

    feat(components): 添加带有文字的二维码组件

---
+++ components/QRCodeWithHint.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import QRCode from 'slidev-addon-qrcode/components/QRCode.vue'
+
+const props = defineProps({
+    ...QRCode.props,
+    hint: String
+})
+</script>
+
+<template>
+    <div class="text-center w-fit">
+        <QRCode class="w-fit" v-bind="$props" :height="props.width" />
+        <a :href="props.data">
+            {{ props.hint }}
+        </a>
+    </div>
+</template>
