2151f21613b6dac41cdce38dae812be3d3de5210
ISNing <ISNing_SF@outlook.com>
2024-08-07 00:18:57 +0800

    feat(components): 添加黑幕组件

---
+++ components/Shadi.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    class: {
+        type: String,
+        default: ''
+    },
+    text: {
+        type: String,
+        default: ''
+    }
+})
+</script>
+
+<template>
+    <div :class="('bg-slate-900 dark:bg-slate-100 w-fit h-fit ' + props.class).trimEnd()">
+      <div class="hover:opacity-100 opacity-0 transition-opacity duration-300 w-fit h-fit">
+        <slot />
+      </div>
+    </div>
+</template>
