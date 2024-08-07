7bcda47f9157a3676c94ae53ac99ee6319a4346b
ISNing <ISNing_SF@outlook.com>
2024-08-08 02:19:12 +0800

    feat(theme/layoutHelper/handleBackground): 增加unocss颜色解析支持

--- theme/layoutHelper.ts
+++ theme/layoutHelper.ts
@@ -1,4 +1,11 @@
 import type { CSSProperties } from 'vue'
+import { theme } from '@unocss/preset-wind'
+
+interface UnoColors {
+    [key: string]: UnoColors & {
+        DEFAULT?: string;
+    } | string;
+}
 
 /**
  * Resolve urls from frontmatter and append with the base url
@@ -10,13 +17,27 @@ export function resolveAssetUrl(url: string) {
 }
 
 export function handleBackground(background?: string, dim = false): CSSProperties {
-  const isColor = background && ['#', 'rgb', 'hsl'].some(v => background.indexOf(v) === 0)
+  if (!background)
+    return {}
 
+  const colorParts = background.split('-')
+  
+  let unboxedColor = theme.colors?.[colorParts[0]]
+  for (let i = 0; i < colorParts.length && unboxedColor; i++) {
+    if (typeof unboxedColor === 'string') {
+      background = unboxedColor
+      break
+    } else if (i === colorParts.length - 1) break
+    else unboxedColor = unboxedColor?.[colorParts[i+1]]
+  }
+
+  const isColor = ['#', 'rgb', 'hsl'].some(v => background.indexOf(v) === 0)
+  
   const style = {
     background: isColor
       ? background
       : undefined,
-    color: (background && !isColor)
+    color: !isColor
       ? 'white'
       : undefined,
     backgroundImage: isColor
