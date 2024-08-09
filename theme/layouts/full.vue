ea77ceacb0376e49bb871e8843c86f9d58e1e153
ISNing <ISNing_SF@outlook.com>
2024-08-10 01:25:06 +0800

    feat(theme): 添加 `full`, `fullpage` 布局

---
+++ theme/layouts/full.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../layoutHelper'
+import SlideLogo from '../components/SlideLogo.vue'
+
+const props = defineProps({
+  ...SlideLogo.props,
+  background: {
+    type: String,
+    default: undefined,
+  },
+})
+
+const style = computed(() => handleBackground(props.background, true))
+</script>
+
+<template>
+  <div class="slidev-layout full w-full h-full" :style="style">
+    <SlideLogo v-bind="$props" />
+    <div class="my-auto">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ theme/layouts/fullpage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../layoutHelper'
+import SlideLogo from '../components/SlideLogo.vue'
+
+const props = defineProps({
+  ...SlideLogo.props,
+  background: {
+    type: String,
+    default: undefined,
+  },
+})
+
+const style = computed(() => handleBackground(props.background, true))
+</script>
+
+<style>
+.fullpage {
+  @apply 'p-0 m-0'
+}
+</style>
+
+<template>
+  <div class="slidev-layout fullpage w-full h-full" :style="style">
+    <SlideLogo v-bind="$props" />
+    <div class="my-auto">
+      <slot />
+    </div>
+  </div>
+</template>
